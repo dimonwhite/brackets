86d3f8c385de1efa2bd09f8e8f0a299cec1254be
dimonwhite <gen-tver@yandex.ru>
2020-03-04 23:25:51 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2) {
+        return false;
+    }
+
+    let boolean = true,
+        error;
+
+    while (str.length > 0) {
+        error = 0;
+        bracketsConfig.forEach(item => {
+            item = item.join('');
+            let index = str.indexOf(item);
+            if (index !== -1) {
+                str = str.replace(item, '');
+            }
+            else {
+                error++;
+            }
+        });
+        if (error === bracketsConfig.length) {
+            boolean = false;
+            break;
+        }
+    }
+
+    return boolean;
 }
